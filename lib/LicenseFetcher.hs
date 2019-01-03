f189255c3c961e76574f7512531dee003bc69223
Martin Buck <buckmartin@buckmartin.de>
2019-01-03 17:56:54 +0100

    fetch and store logic

--- lib/LicenseFetcher.hs
+++ lib/LicenseFetcher.hs
@@ -1,5 +1,30 @@
 module LicenseFetcher where
 
+import Network.HTTP     (simpleHTTP, getRequest, getResponseBody)
+import System.Directory (withCurrentDirectory)
 
-fetch :: Int
-fetch = 3
+--------------------------------------------------------------------------------
+
+newtype Package   = Package   { getPackage   :: String }
+newtype Version   = Version   { getVersion   :: String }
+newtype License   = License   { getLicense   :: String }
+newtype Directory = Directory { getDirectory :: String }
+
+--------------------------------------------------------------------------------
+
+-- | Fetches the license of a versioned package
+fetch :: Package -> Version -> IO License
+fetch (Package p) (Version v) = do
+  response <- simpleHTTP . getRequest $ "http://hackage.haskell.org/package/" ++ p ++ "-" ++ v ++ "/src/LICENSE"
+  body     <- getResponseBody response
+  pure . License $ body
+
+--------------------------------------------------------------------------------
+
+-- | Fetches the license of a versioned package and stores it as file in the provided directory
+fetchStore :: Maybe Directory -> Package -> Version -> IO ()
+fetchStore md pp@(Package p) vv  = do
+  l <- fetch pp vv
+  case md of
+    Nothing            -> writeFile p (getLicense l)
+    Just (Directory d) -> withCurrentDirectory d $ writeFile p (getLicense l) 
--- src/Main.hs
+++ src/Main.hs
@@ -3,5 +3,7 @@ module Main where
 import LicenseFetcher
 
 main :: IO ()
-main = do 
-    putStrLn . show $ fetch
+main = do
+  license <- fetch (Package "attoparsec") (Version "0.13.2.2")
+  putStrLn . getLicense $ license
+  fetchStore (Just . Directory $ "test") (Package "attoparsec") (Version "0.13.2.2")
